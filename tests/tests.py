e53de7910d1c06b170e4b604f18fd85804345c86
degregor69 <raphael.degregorio@gmail.com>
2025-06-13 11:43:58 +0200

    added test of show summary

---
+++ tests/tests.py
@@ -0,0 +1,11 @@
+from .fixtures import client
+from bs4 import BeautifulSoup
+
+def test_show_summary_with_mock_data(client):
+    response = client.post('/showSummary', data={'email': 'mock@club.com'})
+    assert response.status_code == 200
+    soup = BeautifulSoup(response.data, 'html.parser')
+
+    h2 = soup.find('h2')
+    assert h2 is not None
+    assert "mock@club.com" in h2.text
