323c6ae41c7502e7643fc412c87402759ee3d03e
degregor69 <raphael.degregorio@gmail.com>
2025-06-20 07:52:17 +0200

    updated tests.py with new mock

--- tests/tests.py
+++ tests/tests.py
@@ -1,20 +1,20 @@
+import pytest
+
 from .conftest import client
 from bs4 import BeautifulSoup
 
 def test_show_summary_with_mock_data(client):
-    response = client.post('/showSummary', data={'email': 'mock@club.com'})
+    response = client.post('/showSummary', data={'email': 'test@club.com'})
     assert response.status_code == 200
     soup = BeautifulSoup(response.data, 'html.parser')
 
-    h2 = soup.find('h2')
-    assert h2 is not None
-    assert "mock@club.com" in h2.text
+    assert "test@club.com" in soup.text
 
 def test_to_book_more_than_12_tickets(client):
     response = client.post('/purchasePlaces', data={
         'places': 13,
-        'competition': "Mock Competition",
-        'club': 'Mock Club'
+        'competition': "Test Competition",
+        'club': 'Test Club'
     })
     soup = BeautifulSoup(response.data, 'html.parser')
     assert 'Impossible to book more than 12 tickets' in soup.text
