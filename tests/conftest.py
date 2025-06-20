d467afb7019285ca1ffeed151a9e5eeb1872bb44
degregor69 <raphael.degregorio@gmail.com>
2025-06-20 07:49:11 +0200

    splitted test club and test competitions into different fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,21 @@
-
 import pytest
 import server
 
+
 @pytest.fixture
-def client(monkeypatch):
-    test_clubs = [
-        {"name": "Mock Club", "email": "mock@club.com", "points": "15"}
+def test_clubs():
+    return [
+        {"name": "Test Club", "email": "test@club.com", "points": "30"},
     ]
-    test_competitions = [
-        {"name": "Mock Competition", "date": "2025-12-12 10:00:00", "numberOfPlaces": "20"}
+
+@pytest.fixture
+def test_competitions():
+    return [
+        {"name": "Test Competition", "date": "2025-12-31 10:00:00", "numberOfPlaces": "25"},
     ]
+
+@pytest.fixture
+def client(monkeypatch, test_clubs, test_competitions):
     monkeypatch.setattr(server, "clubs", test_clubs)
     monkeypatch.setattr(server, "competitions", test_competitions)
 
