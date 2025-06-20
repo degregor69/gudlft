92576eca96283526f421df749297d0e7f3071739
degregor69 <raphael.degregorio@gmail.com>
2025-06-20 20:31:35 +0200

    clubs and competitions are now separated fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,25 @@
-
 import pytest
 import server
 
+
 @pytest.fixture
-def client(monkeypatch):
+def test_clubs(monkeypatch):
     test_clubs = [
-        {"name": "Mock Club", "email": "mock@club.com", "points": "15"}
-    ]
-    test_competitions = [
-        {"name": "Mock Competition", "date": "2025-12-12 10:00:00", "numberOfPlaces": "20"}
+        {"name": "Test Club", "email": "test@club.com", "points": "30"},
     ]
     monkeypatch.setattr(server, "clubs", test_clubs)
-    monkeypatch.setattr(server, "competitions", test_competitions)
+    return test_clubs
 
+@pytest.fixture
+def test_competitions(monkeypatch):
+    test_competitions = [
+        {"name": "Test Competition", "date": "2025-12-31 10:00:00", "numberOfPlaces": "25"},
+    ]
+    monkeypatch.setattr(server, "competitions", test_competitions)
+    return test_competitions
+
+@pytest.fixture
+def client():
     server.app.config['TESTING'] = True
     with server.app.test_client() as client:
         yield client
--- tests/tests.py
+++ tests/tests.py
@@ -1,11 +1,12 @@
+import pytest
+
 from .conftest import client
 from bs4 import BeautifulSoup
 
-def test_show_summary_with_mock_data(client):
-    response = client.post('/showSummary', data={'email': 'mock@club.com'})
+def test_show_summary_with_mock_data(client, test_clubs, test_competitions):
+    response = client.post('/showSummary', data={'email': 'test@club.com'})
     assert response.status_code == 200
     soup = BeautifulSoup(response.data, 'html.parser')
 
-    h2 = soup.find('h2')
-    assert h2 is not None
-    assert "mock@club.com" in h2.text
+    assert "test@club.com" in soup.text
+
