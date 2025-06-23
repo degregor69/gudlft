46e8dd96ef00d06632dbe52854e53c6859e58815
degregor69 <raphael.degregorio@gmail.com>
2025-06-23 20:44:36 +0200

    added is_competition_in_future function

--- services/services.py
+++ services/services.py
@@ -1,5 +1,13 @@
+from datetime import datetime
+
+
 def validate_places_request(places_requested):
     MAX_PLACES = 12
     if places_requested < 1 or places_requested > MAX_PLACES:
         return False
     return True
+
+def is_competition_in_future(competition):
+    competition_date_str = competition.get('date')
+    competition_date = datetime.strptime(competition_date_str, "%Y-%m-%d %H:%M:%S")
+    return competition_date > datetime.now()
