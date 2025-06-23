d54060beb2965fcdb25ddd6e1eb748774c4c68cb
degregor69 <raphael.degregorio@gmail.com>
2025-06-23 20:57:48 +0200

    test of is competition in future

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -1,7 +1,19 @@
-from services.services import validate_places_request
+from services.services import validate_places_request, is_competition_in_future
+from datetime import datetime, timedelta
 
 def test_validate_places_request():
     assert validate_places_request(0) is False
     assert validate_places_request(1) is True
     assert validate_places_request(12) is True
     assert validate_places_request(13) is False
+
+
+def test_is_competition_in_future_with_future_date():
+    future_date = (datetime.now() + timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S")
+    competition = {"date": future_date}
+    assert is_competition_in_future(competition) is True
+
+def test_is_competition_in_future_with_past_date():
+    past_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S")
+    competition = {"date": past_date}
+    assert is_competition_in_future(competition) is False
